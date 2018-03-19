d7fb56c475cb23be97c87225243beca0cacd28fe
terrancesnyder <terrance.a.snyder@gmail.com>
2018-03-19 11:24:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+PORT=$1
+
 target=/opt/dnsmasq-rest-api/
 
 echo "Installing dnsmasq-rest-api to $target."
@@ -21,8 +23,8 @@ chmod 0440 /etc/sudoers.d/dnsmasq
 echo "Configuring apache2"
 
 a2enmod rewrite
-ln -sf $target/config/apache2/dnsmasq-rest-api.conf /etc/apache2/conf.d/dnsmasq-rest-api.conf
-/etc/init.d/apache2 restart
+ln -sf $target/config/apache2/dnsmasq-rest-api.conf /etc/apache2/conf-enabled/dnsmasq-rest-api.conf
+sudo service apache2 restart
 chown -R www-data $target/zones
 cp $target/www/config.example.php $target/www/config.php
 
@@ -31,14 +33,14 @@ echo "Dnsmasq-rest-api installed."
 echo "Running tests."
 
 echo "* Listing zones"
-curl -s http://localhost/dnsmasq-rest-api/zones | grep "\\[" | grep "\\]"
+curl -s http://localhost:$PORT/dnsmasq-rest-api/zones | grep "\\[" | grep "\\]"
 echo "* Adding records"
-curl -s -X POST http://localhost/dnsmasq-rest-api/zones/myTest/127.0.0.2/localhost.test | grep OK
+curl -s -X POST http://localhost:$PORT/dnsmasq-rest-api/zones/myTest/127.0.0.2/localhost.test | grep OK
 echo "* Reload dnsmasq"
-curl -s -X POST http://localhost/dnsmasq-rest-api/reload | grep OK
+curl -s -X POST http://localhost:$PORT/dnsmasq-rest-api/reload | grep OK
 echo "* Testing dns"
 nslookup localhost.test 127.0.0.1 | grep 127.0.0.2
 echo "* Removing test zone"
-curl -s -X DELETE http://localhost/dnsmasq-rest-api/zones/myTest | grep OK
+curl -s -X DELETE http://localhost:$PORT/dnsmasq-rest-api/zones/myTest | grep OK
 
 echo "Tests ok."
